eeaf383dfaa91bdf8dbb856b9d8fc35d79b4565c
gitquil07 <61176109+gitquil07@users.noreply.github.com>
2026-01-18 15:11:24 +0000

    displaying images with react

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import chef from './images/chef.jpg'
 
 function Header({ name, year }) {
   return (
@@ -22,13 +23,20 @@ const dishObjects = items.map((dish, i) => ({
 
 function Main({ dishes }) {
   return (
-    <ul>
-      {dishes.map((dish) => (
-        <li key={dish.id} style={{ listStyleType: "none" }}>
-          {dish.title}
-        </li>
-      ))}
-    </ul>
+    <main>
+      <img
+        src='https://github.com/eveporcello.png'
+        height={400}
+        alt='A photo of smiling chef owner'
+      />
+      <ul>
+        {dishes.map((dish) => (
+          <li key={dish.id} style={{ listStyleType: "none" }}>
+            {dish.title}
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
